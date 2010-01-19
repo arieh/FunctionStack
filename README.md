8573edd0349ec46c18d8078573e6a4762760fa2a
Arieh <arieh.glazer@gmail.com>
2010-01-19 20:40:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+FunctionStack
+========
+A very simple class that allows stacking functions and calling them step by step or priodiacly.
+
+How to use
+----------
+
+This is quite stright farward:
+
+	#JS
+	var stack = new FunctionStack(
+		function(){console.log('a');},
+		function(){console.log('b');},
+		function(){console.log('c');},
+		function(){console.log('d');},
+		function(){console.log('e');}
+	);
+	
+	stack.step(); //will call log 'a'
+	
+	stack.play(); //will log 'b' 'c' 'd' 'e' periodicaly (defualt to 1000 miliseconds)
+
+Methods
+--------
+
+1. push : recives an unlimited list of functions. adds them to the stack
+2. step : will call the next function in the stack. if any paramaters were sent, will send them to the function
+3. play (`int` duration, `array` args) : will step through the stack priodicaly. if args is supplied, will pass them to the functions
+4. pause : will pause the periodical call
+5. stop : will stop the periodcal call, reseting the counter
+6. reset : will reset the stack index
+7. clear : will clear the stack
+
---
+++ Source/FunctionStack.js
@@ -0,0 +1,79 @@
+/*
+---
+description: A small wrapper to the Chan class to supply easy prodical function calls
+
+license: MIT-style
+
+authors:
+- Arieh Glazer
+
+requires:
+- core/1.2.4: Class
+- core/1.2.4: Class.Extras
+
+provides: FunctionStack
+
+...
+*/
+/*!
+Copyright (c) 2009 Arieh Glazer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE 
+*/
+var FunctionStack = new Class({
+	handle : $empty,
+	list : [(function(){this.stop();}).bind(this)],
+	index : 0,
+	defaultDuration : 1000,
+	initialize: function(){
+		this.push(arguments || []);
+	},
+	push : function(){
+		var func = this.list.pop();
+		this.list.extend($A(arguments).flatten());
+		this.list.push(func);
+	},
+	step : function(){
+		if (this.index < this.list.length) this.list[this.index++].create({'arguments' : arguments || []);
+	},
+	play : function(duration,args){
+		this.handle = this.step.bind(this).periodical(duration || this.defaultDuration, args || []);
+		return this;
+	},
+	stop : function(){
+		$clear(this.handle);
+		this.reset();
+		return this;
+	},
+	pause : function(){
+		$clear(this.handle);
+		return this;
+	},
+	reset : function(){
+		this.index = 0;
+		return this;
+	},
+	clear : function(){
+		var self = this;
+		
+		this.reset();
+		this.list =  [(function(){this.stop();}).bind(this)];
+		return this;
+	}
+});
